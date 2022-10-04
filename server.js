0f67e14ae460f23f4e195d235c47096265e3240e
terrence <terrence.budnik@gmail.com>
2022-10-04 18:24:27 -0500

    set up inquirer

--- server.js
+++ server.js
@@ -5,10 +5,6 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-
-// Connect to database
 const db = mysql.createConnection(
   {
     host: 'localhost',
