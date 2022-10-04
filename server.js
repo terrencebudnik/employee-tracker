f008e9be3323d9a37f40b9a719e9cd45087576b9
terrence <terrence.budnik@gmail.com>
2022-10-04 18:49:24 -0500

    fixed path issues

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mysql = require('mysql2');
+const inquirer = require('inquirer');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -15,7 +16,17 @@ const db = mysql.createConnection(
   console.log(`Connected to the office_db database.`)
 );
 
-
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+inquirer
+  .prompt([
+    {
+      type: 'list',
+      message: "What would you like to do?",
+      name: 'start',
+      options: ["View all departments", "View all roles", "View all employees", "Add a department", "Add a role", "Add an employee", "Update an employee role"]
+    }
+  ]);
+
